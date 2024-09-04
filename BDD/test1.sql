76fa1cdabbb8e4bcc9b6694ba94ba98ef264006b
korasrar <celestin.maubert@etu.univ-orleans.fr>
2024-09-04 16:41:47 +0200

    ajout sql

---
+++ BDD/test1.sql
@@ -0,0 +1,15 @@
+create table BdHabitantVoiture(
+    Prenom Varchar2(20),
+    Nom Varchar2(20),
+    Sexe Varchar2(1),
+    Rue Varchar2(20),
+    Num Number(2),
+    Ville Varchar2(10),
+    MarqueV Varchar2(10),
+    NomV Varchar2(10),
+    AnneeV Number(5),
+    CouleurV Varchar2(10),
+    TypeMoteurV Varchar2(10),
+    ImmV Varchar2(10)
+    ImmV Varchar2(10)
+);
