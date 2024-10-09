9225fb9faa6717a5003f8774af4f141f37a4e448
korasrar <celestin.maubert@etu.univ-orleans.fr>
2024-10-09 15:27:01 +0200

    tp5 updt

--- R101/TP5/tp5_sources.py
+++ R101/TP5/tp5_sources.py
@@ -1,34 +1,104 @@
-def mystere(liste, valeur):
-    """[summary]
+def nbval_3ite(liste, valeur):
+    """Retourne le nombre de valeur parcouru la liste a partir du 
+    moment ou il y aura plus de 3 itérations de la valeurs choisi dans une liste
 
     Args:
-        liste ([type]): [description]
-        valeur ([type]): [description]
+        liste (list): liste de valeur
+        valeur (int): valeur
 
     Returns:
-        [type]: [description]
+        Bool or Int: None ou cpt si la valeur apparée plus de 3 fois
     """
-    xxx = 0
-    yyy = 0
-    for elem in liste:
-        if elem == valeur:
-            yyy += 1
-            if yyy > 3:
-                return xxx
-        xxx += 1
+    cpt = 0
+    equalval = 0
+    for i in range(len(liste)):
+        if liste[i] == valeur:
+            equalval += 1
+            if equalval > 3:
+                return cpt
+        cpt += 1
     return None
+#print(mystere([12, 20, 8, 48, 20, 418, 20, 17, 20, 87], 20))
 
+def test_nbval_itevaleurs():
+    assert nbval_itevaleurs([],) == None
+    assert nbval_itevaleurs([],) == None
+    assert nbval_itevaleurs([],) == None
+    assert nbval_itevaleurs([],) == None
 
-mystere([12, 5, 8, 48, 12, 418, 185, 17, 5, 87], 20)
+# --------------------------------------
+# Exercice n°2
+# --------------------------------------
+
+def indfirstint(phrase) :
+    for i in range(len(phrase)) :
+        if phrase[i].isnumeric() :
+            return i
+    return None
+#print(indfirstint("shinjiikari"))
+
+def test_indfirstint():
+    assert indfirstint("") == None
+    assert indfirstint("") == None
+    assert indfirstint("") == None
+    assert indfirstint("") == None    
+
+def cityfinder(liste_ville,popu,ville) : 
+    for i in range(len(liste_ville)) :
+        if liste_ville[i] == ville :
+            return popu[i]
+    return None
+#print(cityfinder(["Blois", "Bourges", "Chartres", "Châteauroux", "Dreux","Joué-lès-Tours", "Olivet", "Orléans", "Tours", "Vierzon"],[45871, 64668,  38426, 43442, 30664, 38250, 22168, 116238,136463,  25725],"Blois"))
 
 # --------------------------------------
 # Exemple de villes avec leur population
 # --------------------------------------
-liste_villes = ["Blois", "Bourges", "Chartres", "Châteauroux", "Dreux",
-                "Joué-lès-Tours", "Olivet", "Orléans", "Tours", "Vierzon"]
-population = [45871, 64668,  38426, 43442, 30664, 38250, 22168, 116238,
-                136463,  25725]
+liste_villes = ["Blois", "Bourges", "Chartres", "Châteauroux", "Dreux","Joué-lès-Tours", "Olivet", "Orléans", "Tours", "Vierzon"]
+population = [45871, 64668,  38426, 43442, 30664, 38250, 22168, 116238,136463,  25725]
 
+# --------------------------------------
+# Exercice n°3
+# --------------------------------------
+
+def listcroissant(list_nb):
+    for i in range(1,len(list_nb)):
+        if list_nb[i] > list_nb[i-1] :
+            res = True
+        else :
+            return False
+    return res
+#print(listcroissant([1,2,3,4,5,6,7,8,9]))
+
+def test_listcroissant():
+    assert listcroissant([]) == False
+    assert listcroissant([]) == False
+    assert listcroissant([]) == False
+    assert listcroissant([]) == False
+
+def sommesupvaleurs(list_nb,valeur) :
+    somme = 0
+    for i in range(len(list_nb)):
+        somme += list_nb[i]
+    if somme > valeur :
+        return True
+    return False
+#print(sommesupvaleurs([1,4,1,2,3,4],6))
+
+def test_sommesupvaleurs():
+    assert sommesupvaleurs([],) == True
+    assert sommesupvaleurs([],) == True
+    assert sommesupvaleurs([],) == True
+    assert sommesupvaleurs([],) == True
+
+def verifemail(email):
+    if phrase[0] != "@" and phrase[-1] != "." :   
+        for i in range(len(email)) :
+            f
+    return False
+
+# ---------------------------------------
+# Exercice n°4 
+# ---------------------------------------
 # ---------------------------------------
 # Exemple de scores
 # ---------------------------------------
