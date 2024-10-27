2a0811c4b4adbe1ed41cf2cd0519866f0342740a
korasrar <celestin.maubert@etu.univ-orleans.fr>
2024-10-27 16:48:25 +0100

    ptn c'est fou la

--- SAE2/Bilan_Carbone_mysterieux_20241002/consultation_activites.py
+++ SAE2/Bilan_Carbone_mysterieux_20241002/consultation_activites.py
@@ -12,7 +12,7 @@ def afficher_menu(titre, liste_options):
     print("|",titre,"|")
     print("+-------------------------+")
     for i in range(len(liste_options)):
-        print(i+1," -> ",liste_options[i])
+        print("[",i+1,"]",liste_options[i])
 
 def demander_nombre(message, borne_max):
     rep = int(input(message))
@@ -24,6 +24,14 @@ def menu(titre, liste_options):
     afficher_menu(titre,liste_options)
     repasknb = demander_nombre("Entrez votre choix [1-"+str(len(liste_options))+"] : ",len(liste_options))
     return repasknb
+
+def askname(liste_chargée):
+    while True:
+        name = input("Entrer le nom de la personne : ")
+        for activite in liste_chargée:
+            if name == activite[0]:
+                return name
+        print("Nom introuvable dans la liste. Réessayer.")
 
 # ici votre programme principal
 def programme_principal():
@@ -46,27 +54,29 @@ def programme_principal():
         else:
             fichierchargé = True
             print("FICHIER CHARGER")
+    askname
     while not quitter:
         rep = menu("MENU DE MON APPLICATION", liste_options)
         if rep is None:
             print("Cette option n'existe pas")
-        elif rep == 1:
+        elif rep == 1: # BILAN CARBONE MOIS DE SEPTEMBRE -------------------
             print("Vous avez choisi", liste_options[rep - 1])
-            nom = input("Entrer le nom de la personne : ")
-            print(bc.cumul_emmissions(bc.filtre(liste_chargée,0,nom)))
-        elif rep == 2:
+            nom = askname(liste_chargée)
+            print("Bilan carbone de ",nom," : ",bc.cumul_emmissions(bc.filtre(liste_chargée,0,nom)))
+        elif rep == 2: # LISTE ACTIVITE ------------------------------------
             print("Vous avez choisi", liste_options[rep - 1])
-        elif rep == 3:
-            print("Vous avez choisi", liste_options[rep - 1])
-            nom = input("Entrer le nom de la personne : ")
+            nom = askname(liste_chargée)
             print(bc.filtre(liste_chargée,0,nom))
             saveliste = bc.filtre(liste_chargée,0,nom)
+        elif rep == 3:
+            print("Vous avez choisi", liste_options[rep - 1])
         elif rep == len(liste_options):
             quitter = True
         if saveliste != []:
-            if input("Voulez vous sauvegarder la liste créer dans un fichier csv ? (y/n) : ") == y:
+            if input("Voulez vous sauvegarder la liste créer dans un fichier csv ? (y/n) : ") == 'y':
                 bc.sauver_activites(input("Entrer le nom du fichier : ",saveliste))
                 saveliste = []
+                print("Fichier sauvegarder !")
         input("Appuyer sur Entrée pour continuer")
     print("Merci au revoir!")
 
