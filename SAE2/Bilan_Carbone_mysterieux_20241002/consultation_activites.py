9bd6ecc6ef36964f931ce1da6de77b804f05f5e3
korasrar <celestin.maubert@etu.univ-orleans.fr>
2024-10-30 12:50:42 +0100

    push

--- SAE2/Bilan_Carbone_mysterieux_20241002/consultation_activites.py
+++ SAE2/Bilan_Carbone_mysterieux_20241002/consultation_activites.py
@@ -6,7 +6,7 @@ def afficher_menu(titre, liste_options):
 
     Args:
         titre (str): Titre de l'application
-        liste_options (list): Liste des options a sélectionner par l'utilisateur
+        liste_optionsmenu (list): Liste des options a sélectionner par l'utilisateur
     """    
     print("+-------------------------+")
     print("|",titre,"|")
@@ -20,86 +20,133 @@ def demander_nombre(message, borne_max):
         return int(rep)
     return None
 
-def menu(titre, liste_options):
-    afficher_menu(titre,liste_options)
-    repasknb = demander_nombre("Entrez votre choix [1-"+str(len(liste_options))+"] : ",len(liste_options))
+def menu(titre, liste_optionsmenu):
+    afficher_menu(titre,liste_optionsmenu)
+    repasknb = demander_nombre("Entrez votre choix [1-"+str(len(liste_optionsmenu))+"] : ",len(liste_optionsmenu))
     return repasknb
 
-def askname(liste_chargée):
-    while True:
+def askname(liste_chargee):
+    check = False
+    while not check:
         name = input("Entrer le nom de la personne : ")
-        for activite in liste_chargée:
+        for activite in liste_chargee:
             if name == activite[0]:
-                return name
+                check = True
         print("Nom introuvable dans la liste. Réessayer.")
+    return name
 
-def asktype(liste_chargée):
-    while True:
-        type = input("Entrer le type a cherhcer : ")
-        for activite in liste_chargée:
+def asktype(liste_chargee):
+    check = False
+    while not check:
+        type = input("Entrer le type a chercher : ")
+        for activite in liste_chargee:
             if type == activite[3]:
-                return type
+                check = True
         print("Type introuvable dans la liste. Réessayer.")
+    return type
+
+def askdate(liste_chargee):
+    check = False
+    while not check:
+        date = input("Entrer la date a chercher : ")
+        for activite in liste_chargee:
+            if date == activite[3]:
+                check = True
+        print("Type introuvable dans la liste. Réessayer.")
+    return date
 
 def loadfile():
+    liste_chargee = []
     fichierchargé = False
     while not fichierchargé :
         try:
-            liste_chargée = bc.charger_activites(input("Veuillez entrer le nom d'un fichier csv : "))
+            liste_chargee = bc.charger_activites(input("Veuillez entrer le nom d'un fichier csv : "))
         except:
             print("Fichier inexistant, Recommencer !")
         else:
             fichierchargé = True
             print("FICHIER CHARGER !")
+            return liste_chargee
+
+def askfiltre(liste_options,liste_chargee):
+    quitter = False
+    while not quitter :
+        rep = menu("CHOIX DU FILTRE",liste_options)
+        if rep is None:
+            print("Ce filtre n'existe pas")
+        elif rep == 1:
+            filtre = askname(liste_chargee)
+            res = bc.filtre(liste_chargee,0,filtre)
+            quitter = True
+        elif rep == 2:
+            filtre = askdate(liste_chargee)
+            res = bc.filtre(liste_chargee,0,filtre)
+            quitter = True
+        elif rep == 3:
+            ask
+            res = a
+            quitter = True
+        elif rep == 4:
+            filtre = asktype(liste_chargee)
+            res = bc.filtre(liste_chargee,3,filtre)
+            quitter = True
+    return res
 
 
 # ici votre programme principal
 def programme_principal():
-    liste_options = [
+    liste_optionsmenu = [
                 "Bilan Carbone pour septembre",
-                "Liste activité",
+                "Liste activité avec filtre",
                 "Activité la plus polluante",
                 "Moyenne émission de carbone en septembre",
                 "Pourcentage de personne pratiquant une activité",
                 "Tendance",
                 "Charger un nouveaux fichier",
                 "Quitter"]
-    liste_chargée = []
+    liste_optionsfiltre = [
+                "Prénom",
+                "Type",
+                "Date (year-month-day)",
+                "> ou < Carbone (ex : < 40)"]
+    liste_chargee = []
     saveliste = []
+    listerfusion = []
     quitter = False
-    loadfile()
+    liste_chargee = loadfile()
     while not quitter:
-        rep = menu("MENU DE MON APPLICATION", liste_options)
+        rep = menu("MENU DE MON APPLICATION", liste_optionsmenu)
         if rep is None:
             print("Cette option n'existe pas")
         elif rep == 1: # BILAN CARBONE MOIS DE SEPTEMBRE -------------------
-            print("Vous avez choisi", liste_options[rep - 1])
-            nom = askname(liste_chargée)
-            print("Bilan carbone de ",nom," : ",bc.cumul_emmissions(bc.filtre(liste_chargée,0,nom)),"g")
+            print("Vous avez choisi", liste_optionsmenu[rep - 1])
+            nom = askname(liste_chargee)
+            print("Bilan carbone de ",nom," : ",bc.cumul_emmissions(bc.filtre(liste_chargee,0,nom)),"g")
         elif rep == 2: # LISTE ACTIVITE ------------------------------------
-            print("Vous avez choisi", liste_options[rep - 1])
-            nom = askname(liste_chargée)
-            print(bc.filtre(liste_chargée,0,nom))
-            saveliste = bc.filtre(liste_chargée,0,nom)
+            print("Vous avez choisi", liste_optionsmenu[rep - 1])
+            filtre = askfiltre(liste_optionsfiltre,liste_chargee)
+            print(bc.filtre(liste_chargee,0,filtre))
+            saveliste = bc.filtre(liste_chargee,0,nom)
         elif rep == 3: # ACTIVITE LA PLUS POLLUANTE ------------------------
-            print("Vous avez choisi", liste_options[rep - 1])
-            nom = askname(liste_chargée)
-            print(bc.max_emmission(bc.filtre(liste_chargée,0,nom)))
+            print("Vous avez choisi", liste_optionsmenu[rep - 1])
+            nom = askname(liste_chargee)
+            print(bc.max_emmission(bc.filtre(liste_chargee,0,nom)))
         elif rep == 4: # MOYENNE EMISSION SEPTEMBRE ------------------------
-            print("Vous avez choisi", liste_options[rep - 1])
-            nom = askname(liste_chargée)
-            filtre = bc.filtre(liste_chargée,0,nom)
+            print("Vous avez choisi", liste_optionsmenu[rep - 1])
+            nom = askname(liste_chargee)
+            filtre = bc.filtre(liste_chargee,0,nom)
             print("Moyenne de",nom,"sur septembre : ",bc.cumul_emmissions(filtre)//len(filtre),"g")
         elif rep == 5: # POURCENTAGE ACTIVITE ------------------------------
-            print("Vous avez choisi", liste_options[rep - 1])
-            type = asktype(liste_chargée)
-            print((len(bc.filtre(liste_chargée,3,type))//len(liste_chargée))*100,"% des activitées qui sont de",type)
+            print("Vous avez choisi", liste_optionsmenu[rep - 1])
+            type = asktype(liste_chargee)
+            pourcentage = (len(bc.filtre(liste_chargee,3,type))/len(liste_chargee))*100
+            print(round(pourcentage,2),"% des activitées qui sont de",type)
         elif rep == 6: # TENDANCE ------------------------------
-            print("Vous avez choisi", liste_options[rep - 1])
+            print("Vous avez choisi", liste_optionsmenu[rep - 1])
         elif rep == 7: # CHARGER NOUVEAU FICHIER ---------------------------
-            print("Vous avez choisi", liste_options[rep - 1])
+            print("Vous avez choisi", liste_optionsmenu[rep - 1])
             loadfile()
-        elif rep == len(liste_options):
+        elif rep == len(liste_optionsmenu):
             quitter = True
         if saveliste != []:
             if input("Voulez vous sauvegarder la liste créer dans un fichier csv ? (y/n) : ") == 'y':
