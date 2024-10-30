b48b51e149fe58f55afe8a8f318a7192cecff889
korasrar <celestin.maubert@etu.univ-orleans.fr>
2024-10-30 16:12:47 +0100

    updt

--- SAE2/Bilan_Carbone_mysterieux_20241002/consultation_activites.py
+++ SAE2/Bilan_Carbone_mysterieux_20241002/consultation_activites.py
@@ -35,6 +35,16 @@ def askname(liste_chargee):
         print("Nom introuvable dans la liste. Réessayer.")
     return name
 
+def askdate(liste_chargee):
+    check = False
+    while not check:
+        date = input("Entrer la date a chercher : ")
+        for activite in liste_chargee:
+            if date == activite[1]:
+                check = True
+        print("Date introuvable dans la liste. Réessayer.")
+    return date
+
 def asktype(liste_chargee):
     check = False
     while not check:
@@ -44,16 +54,6 @@ def asktype(liste_chargee):
                 check = True
         print("Type introuvable dans la liste. Réessayer.")
     return type
-
-def askdate(liste_chargee):
-    check = False
-    while not check:
-        date = input("Entrer la date a chercher : ")
-        for activite in liste_chargee:
-            if date == activite[3]:
-                check = True
-        print("Type introuvable dans la liste. Réessayer.")
-    return date
 
 def loadfile():
     liste_chargee = []
@@ -80,13 +80,9 @@ def askfiltre(liste_options,liste_chargee):
             quitter = True
         elif rep == 2:
             filtre = askdate(liste_chargee)
-            res = bc.filtre(liste_chargee,0,filtre)
+            res = bc.filtre(liste_chargee,1,filtre)
             quitter = True
         elif rep == 3:
-            ask
-            res = a
-            quitter = True
-        elif rep == 4:
             filtre = asktype(liste_chargee)
             res = bc.filtre(liste_chargee,3,filtre)
             quitter = True
@@ -106,9 +102,8 @@ def programme_principal():
                 "Quitter"]
     liste_optionsfiltre = [
                 "Prénom",
-                "Type",
                 "Date (year-month-day)",
-                "> ou < Carbone (ex : < 40)"]
+                "Type"]
     liste_chargee = []
     saveliste = []
     listerfusion = []
@@ -124,9 +119,9 @@ def programme_principal():
             print("Bilan carbone de ",nom," : ",bc.cumul_emmissions(bc.filtre(liste_chargee,0,nom)),"g")
         elif rep == 2: # LISTE ACTIVITE ------------------------------------
             print("Vous avez choisi", liste_optionsmenu[rep - 1])
-            filtre = askfiltre(liste_optionsfiltre,liste_chargee)
-            print(bc.filtre(liste_chargee,0,filtre))
-            saveliste = bc.filtre(liste_chargee,0,nom)
+            listefiltre = askfiltre(liste_optionsfiltre,liste_chargee)
+            print(listefiltre)
+            saveliste = listefiltre
         elif rep == 3: # ACTIVITE LA PLUS POLLUANTE ------------------------
             print("Vous avez choisi", liste_optionsmenu[rep - 1])
             nom = askname(liste_chargee)
