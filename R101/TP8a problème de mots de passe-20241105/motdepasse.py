1df7270093db4e7b5cb4d3cbf38bfd10373dc19c
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-11-05 09:51:48 +0100

    tp8

--- R101/TP8a problème de mots de passe-20241105/motdepasse.py
+++ R101/TP8a problème de mots de passe-20241105/motdepasse.py
@@ -1,33 +1,33 @@
 # Codé par Papy Force X, jeune padawan de l'informatique
+def longueur_ok(mdp):
+    if len(mdp) < 8:
+        return False
+    return True
+
+def chiffre_ok(mdp):
+    for lettre in mdp:
+        if lettre.isdigit():
+            return True
+    return False
+
+def sans_espace(mdp):
+    for lettre in mdp:
+        if lettre == " ":
+            return False
+    return True
 
 def dialogue_mot_de_passe():
     login = input("Entrez votre nom : ")
     mot_de_passe_correct = False
     while not mot_de_passe_correct :
         mot_de_passe = input("Entrez votre mot de passe : ")
-        # je vérifie la longueur
-        if len(mot_de_passe) < 8:
-            longueur_ok = False;
-        else:
-            longueur_ok = True
-        # je vérifie s'il y a un chiffre
-        chiffre_ok = False
-        for lettre in mot_de_passe:
-            if lettre.isdigit():
-                chiffre_ok = True
-        # je vérifie qu'il n'y a pas d'espace
-        sans_espace = True
-        for lettre in mot_de_passe:
-            if lettre == " ":
-                sans_espace = False
-        # Je gère l'affichage
-        if not longueur_ok:
+        if not longueur_ok(mot_de_passe) :
             print("Votre mot de passe doit comporter au moins 8 caractères")
-        elif not chiffre_ok:
+        if not chiffre_ok(mot_de_passe) :
             print("Votre mot de passe doit comporter au moins un chiffre")
-        elif not sans_espace:
-            print("Votre mot de passe ne doit pas comporter d'espace")	   
-        else:
+        if not sans_espace(mot_de_passe):
+            print("Votre mot de passe ne doit pas comporter d'espace")	 
+        if sans_espace(mot_de_passe) and longueur_ok(mot_de_passe) and chiffre_ok(mot_de_passe):
             mot_de_passe_correct = True        
     print("Votre mot de passe est correct")
     return mot_de_passe
