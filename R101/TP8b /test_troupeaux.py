a51bc5cf1e58709485738c0679e86499bdd7d28c
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-11-07 09:36:49 +0100

    tp8b

---
+++ R101/TP8b /test_troupeaux.py
@@ -0,0 +1,79 @@
+import troupeaux
+
+
+def test_total_animaux():
+    troupeau_de_jean = {'vache':12, 'cochon':17, 'veau':3}
+    troupeau_vide = dict()
+    troupeau_de_perrette = {'veau':14, 'vache':7, 'poule':42}
+    mon_troupeau = ...
+    assert troupeaux.total_animaux(troupeau_de_perrette) == 63
+    assert troupeaux.total_animaux(troupeau_de_jean) == 32
+    assert troupeaux.total_animaux(troupeau_vide) == 0
+    assert troupeaux.total_animaux(mon_troupeau) == ...
+
+
+
+def test_tous_les_animaux():
+    jean = {'vache':12, 'cochon':17, 'veau':3}
+    vide = dict()
+    perrette = {'veau':14, 'vache':7, 'poule':42}
+    mon_troupeau = ...    
+    assert troupeaux.tous_les_animaux(perrette) == {'veau', 'vache', 'poule'}
+    assert troupeaux.tous_les_animaux(jean) == {'veau', 'vache', 'cochon'}
+    assert troupeaux.tous_les_animaux(vide) == set()
+    assert troupeaux.tous_les_animaux(mon_troupeau) == ...
+
+
+def test_specialise():
+    troupeau_de_jean = {'vache':12, 'cochon':17, 'veau':3}
+    troupeau_vide = dict()
+    troupeau_de_perrette = {'veau':14, 'vache':7, 'poule':42}
+    mon_troupeau = ...    
+    assert troupeaux.specialise(troupeau_de_perrette)
+    assert not troupeaux.specialise(troupeau_de_jean)
+    assert not troupeaux.specialise(troupeau_vide)
+    assert ...
+
+
+def test_quantite_suffisante():
+    troupeau_de_jean = {'vache':12, 'cochon':17, 'veau':3}
+    troupeau_vide = dict()
+    troupeau_de_perrette = {'veau':14, 'vache':7, 'poule':42}
+    mon_troupeau = ...    
+    assert troupeaux.quantite_suffisante(troupeau_de_perrette)
+    assert not troupeaux.quantite_suffisante(troupeau_de_jean)
+    assert troupeaux.quantite_suffisante(troupeau_vide)
+    assert ...
+
+
+def test_le_plus_represente():
+    troupeau_de_jean = {'vache':12, 'cochon':17, 'veau':3}
+    troupeau_vide = dict()
+    troupeau_de_perrette = {'veau':14, 'vache':7, 'poule':42}
+    mon_troupeau = ...    
+    assert troupeaux.le_plus_represente(troupeau_de_perrette) == 'poule'
+    assert troupeaux.le_plus_represente(troupeau_de_jean) == "cochon"
+    assert troupeaux.le_plus_represente(troupeau_vide) is None
+    assert troupeaux.le_plus_represente(mon_troupeau) == ...
+
+
+def test_reunion_troupeaux():
+    troupeau_de_jean = {'vache':12, 'cochon':17, 'veau':3}
+    troupeau_de_perrette = {'veau':14, 'vache':7, 'poule':42}
+    troupeau_vide = dict()    
+    mon_troupeau = ...
+    assert troupeaux.reunion_troupeaux(troupeau_de_perrette, troupeau_vide) == troupeau_de_perrette
+    assert troupeaux.reunion_troupeaux(troupeau_vide, troupeau_de_jean) == troupeau_de_jean
+    assert troupeaux.reunion_troupeaux(troupeau_de_perrette, troupeau_de_jean) == {'vache':12+7, 'cochon':17, 'veau':3+14, 'poule':42}
+    assert troupeau_de_jean == {'vache':12, 'cochon':17, 'veau':3}
+    assert troupeau_de_perrette == {'veau':14, 'vache':7, 'poule':42}
+    assert troupeaux.reunion_troupeaux(troupeau_de_perrette, mon_troupeau) == ...
+    assert troupeaux.reunion_troupeaux(mon_troupeau, troupeau_de_jean) == ...
+    assert troupeau_de_jean == {'vache':12, 'cochon':17, 'veau':3}
+    assert troupeau_de_perrette == {'veau':14, 'vache':7, 'poule':42}
+
+
+
+
+
+
---
+++ R101/TP8b /troupeaux.py
@@ -0,0 +1,80 @@
+# TP8 B - Manipuler des listes, ensembles et dictionnaires
+
+
+def total_animaux(troupeau):
+    """ Calcule le nombre total d'animaux dans un troupeau
+
+    Args:
+        troupeau (dict): un dictionnaire modélisant un troupeau {nom_animaux: nombre}
+
+    Returns:
+        int: le nombre total d'animaux dans le troupeau
+    """
+    ...
+
+
+def tous_les_animaux(troupeau):
+    """ Détermine l'ensemble des animaux dans un troupeau
+
+    Args:
+        troupeau (dict): un dictionnaire modélisant un troupeau {nom_animaux: nombre}
+
+    Returns:
+        set: l'ensemble des animaux du troupeau
+    """
+    ...
+
+
+def specialise(troupeau):
+    """ Vérifie si le troupeau contient 30 individus ou plus d'un même type d'animal 
+
+    Args:
+        troupeau (dict): un dictionnaire modélisant un troupeau {nom_animaux: nombre}
+
+    Returns:
+        bool: True si le troupeau contient 30 (ou plus) individus d'un même type d'animal,
+        False sinon 
+    """
+    ...
+
+
+def le_plus_represente(troupeau):
+    """ Recherche le nom de l'animal qui a le plus d'individus dans le troupeau
+    
+    Args:
+        troupeau (dict): un dictionnaire modélisant un troupeau {nom_animaux: nombre}
+
+    Returns:
+        str: le nom de l'animal qui a le plus d'individus  dans le troupeau
+        None si le troupeau est vide) 
+    
+    """
+    ...
+
+
+def quantite_suffisante(troupeau):
+    """ Vérifie si le troupeau contient au moins 5 individus de chaque type d'animal
+
+    Args:
+        troupeau (dict): un dictionnaire modélisant un troupeau {nom_animaux: nombre}
+
+    Returns:
+        bool: True si le troupeau contient au moins 5 individus de chaque type d'animal
+        False sinon    
+    """
+    ...
+
+
+def reunion_troupeaux(troupeau1, troupeau2):
+    """ Simule la réunion de deux troupeaux
+
+    Args:
+        troupeau1 (dict): un dictionnaire modélisant un premier troupeau {nom_animaux: nombre}
+        troupeau2 (dict): un dictionnaire modélisant un deuxième troupeau        
+
+    Returns:
+        dict: le dictionnaire modélisant la réunion des deux troupeaux    
+    """
+    ...
+
+
