73df2fc3ef6fc69cfe583223a70a661a1bd4dfe5
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-11-07 14:53:48 +0100

    super heros

--- R101/TP8b /super_heros.py
+++ R101/TP8b /super_heros.py
@@ -10,17 +10,50 @@ def intelligence_moyenne(personnages):
     """Calcul la moyenne des intelligences des personnages
 
     Args:
-        personnages (dico): Liste le personneages avec leur force intelligence et desc
+        personnages (dico): Liste de personnages avec leur force intelligence et desc
 
     Returns:
         float: moyenne intelligence
     """    
     somme = 0
     cpt = 0
-    for intelligence in personnages.values():
-        somme += intelligence[1]
+    for info in personnages.values():
+        somme += info[1]
         cpt += 1
     if cpt > 0 :
         return somme/cpt
     return None
+
+def kikelplusfort(personnages):
+    """Renvoie le nom du perso le plus fort
+
+    Args:
+        personnages (dico): Liste de personnages avec leur force intelligence et desc
+
+    Returns:
+        str: Nom du perso le plus fort
+    """    
+    valforce = 0
+    nomperso = ""
+    for perso,info in personnages.items():
+        if valforce < info[0]:
+            valforce = info[0]
+            nomperso = perso
+    return nomperso
+
+def combienDeCretins(personnages):
+    """Renvoie le nombre de p ersonnages dont l'intelligence est strictement inférieure à la moyenne
+
+    Args:
+        personnages (dico): Liste de personnages avec leur force intelligence et desc
+
+    Returns:
+        int: Nombre de perso qui a un int en dessous de la moyenne
+    """    
+    cpt = 0
+    moyenneint = intelligence_moyenne(personnages)
+    for info in personnages.values():
+        if moyenneint > info[1]:
+            cpt += 1
+    return cpt
 
