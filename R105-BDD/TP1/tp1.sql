6e989644ab0921dad8dbc72b391ca5f58d7e513e
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-11-15 10:57:36 +0100

    tp1 bd

--- R105-BDD/TP1/tp1.sql
+++ R105-BDD/TP1/tp1.sql
@@ -1,23 +1,113 @@
--- rlwrapsqlplus maubert@ora12
+-- rlwrap sqlplus maubert@ora12
 -- drop table nomTable;
 -- select * from tab;
 -- purge recyclebin;
+drop table contenir;
+drop table expediteur;
+drop table colis;
+drop table commande;
+drop table article;
+drop table client;
+drop table pointdistribution;
+purge recyclebin;
 
-create table CLIENT (
+create table pointdistribution (
+    refpointdist number(10),
+    adressepointdist varchar2(30),
+    constraint clepointdist PRIMARY KEY (refpointdist)
+);
+
+create table client (
     codecl number(10),
     nomcl varchar2(30),
     prenom varchar2(30),
     adressecl varchar2(50),
-    emailcl varchar2(30)
-)
+    emailcl varchar2(30),
+    refpointdist number(10),
+    constraint cleclient primary key (codecl),
+    constraint foreignkey_client_refpoindist FOREIGN KEY (refpointdist) REFERENCES pointdistribution (refpointdist) 
+);
 
-create table ATICLE(
+create table article(
     refarticle number(10),
     nomarticle varchar2(35),
     prixht float(8),
-    tva number(2),
-    qtestock number(35)
+    tva number(4, 2),
+    qtestock number(35),
+    constraint cleart primary key (refarticle)
 );
 
+create table commande(
+    numcom number(10),
+    datecom date,
+    codecl number(10),
+    constraint clecom primary key (numcom)
+);
 
-insert into client values(12,'Ayanami','Rei','38 rue du crampté','reiayanami@nerv.com')
+create table colis(
+    numcom number(10),
+    numcolis number(10),
+    indretrait varchar2(2),
+    constraint clecolis primary key (numcolis, numcom),
+    constraint foreignkey_colis_numcom FOREIGN KEY (numcom) REFERENCES commande (numcom)
+);
+
+create table expediteur(
+    numcom number(10),
+    numcolis number(10),
+    refarticle number(10),
+    qteexp number(10),
+    qteacpt number(10), 
+    constraint cleexpediteur primary key (numcolis, numcom, refarticle),
+    constraint foreignkey_expediteur_numcolis FOREIGN KEY (numcolis,numcom) REFERENCES colis (numcolis,numcom),
+    constraint foreignkey_expediteur_refarticle FOREIGN KEY (refarticle) REFERENCES article (refarticle)
+);
+
+create table contenir(
+    numcom number(10),
+    refarticle number(10),
+    qtecom number(10), 
+    constraint clecontenir primary key (refarticle, numcom),
+    constraint foreignkey_contenir_numcom FOREIGN KEY (numcom) REFERENCES commande (numcom),
+    constraint foreignkey_contenir_refarticle FOREIGN KEY (refarticle) REFERENCES article (refarticle)
+);
+
+insert into pointdistribution values(
+    '123',
+    '5 rue de la nerv'
+);
+
+insert into pointdistribution values(
+    '124',
+    '6 rue de la nerv'
+);
+
+insert into pointdistribution values(
+    '125',
+    '7 rue de la nerv'
+);
+
+insert into client values(
+    12,
+    'Ayanami',
+    'Rei',
+    '38 rue du crampte',
+    'reiayanami@nerv.com',
+    '123'
+);
+
+insert into article values(
+    1,
+    'Figurine EVA01',
+    25,
+    20.00,
+    35
+);
+
+insert into article values(
+    2,
+    'Figurine EVA02',
+    30,
+    20.00,
+    20
+);
