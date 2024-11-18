7a206ba4ec5e5f83d96c3c98ca75a8833d769438
korasrar <celestin.maubert@etu.univ-orleans.fr>
2024-11-18 13:30:31 +0100

    caca

--- SAE3/script.sql
+++ SAE3/script.sql
@@ -1,7 +1,20 @@
+-- rlwrap sqlplus maubert@ora12
+-- drop table nomTable;
+-- select * from tab;
+-- purge recyclebin;
+drop table contenir;
+drop table expediteur;
+drop table colis;
+drop table commande;
+drop table article;
+drop table client;
+drop table pointdistribution;
+purge recyclebin;
+
 CREATE TABLE Emplacement (
     PRIMARY KEY (Code_Emplacement),
     Code_Emplacement VARCHAR2(8),
-    IDreservbota     NUMBER(),
+    IDreservbota     NUMBER(10),
     UNIQUE (IDreservbota)
 );
 
@@ -15,14 +28,14 @@ CREATE TABLE Especes (
     Nom_Famille      VARCHAR2(255)
 );
 
-CREATE TABLE Famille_Especes (
+CREATE TABLE FamilleEspeces (
     PRIMARY KEY (Nom_Famille),
     Nom_Famille      VARCHAR2(255),
-    Description      TEXT,
+    Description      VARCHAR2(255),
     Caracteristiques VARCHAR2(42)
 );
 
-CREATE TABLE Fiches_Arrosages (
+CREATE TABLE FichesArrosages (
     PRIMARY KEY (IDFiche),
     IDFiche                        VARCHAR2(42),
     Quantitte_Eau_Semaine          VARCHAR2(42),
@@ -34,7 +47,7 @@ CREATE TABLE IS_A (
     PRIMARY KEY (Superficie, Nom_Orga, IDreservbota),
     Superficie   NUMBNER(10),
     Nom_Orga     VARCHAR2(255),
-    IDreservbota NUMBER()
+    IDreservbota NUMBER(10)
 );
 
 CREATE TABLE Jardins (
@@ -46,17 +59,17 @@ CREATE TABLE Jardins (
 
 CREATE TABLE Nourri (
     PRIMARY KEY (IDEspeces, IDNutriments),
-    IDEspeces    VARCHAR2(42),
-    IDNutriments VARCHAR2(42)
+    IDEspeces   VARCHAR2(42),
+    IDNutriments    VARCHAR2(42)
 );
 
 CREATE TABLE Nutriment (
     PRIMARY KEY (IDNutriments),
-    IDNutriments      VARCHAR2(42),
-    Nom_Nutriments    VARCHAR2(255),
-    Formule_Chimique  VARCHAR2(42),
-    Type              VARCHAR2(42),
-    Taux_Remplacement VARCHAR2(42)
+    IDNutriments    VARCHAR2(42),
+    Nom_Nutriments  VARCHAR2(255),
+    Formule_Chimique    VARCHAR2(42),
+    Type    VARCHAR2(42),
+    Taux_Remplacement   VARCHAR2(42)
 );
 
 CREATE TABLE Plante (
@@ -64,7 +77,7 @@ CREATE TABLE Plante (
     IDPlante         VARCHAR2(42),
     Dateplantation   VARCHAR2(42),
     Couleur          VARCHAR2(50),
-    Hauteur          DECIMAL(10,2),
+    Hauteur          NUMBER(10,2),
     Emplacement      VARCHAR2(42),
     Code_Emplacement VARCHAR2(8)
 );
@@ -75,9 +88,9 @@ CREATE TABLE Remplacer (
     IDNutriments_2 VARCHAR2(42)
 );
 
-CREATE TABLE Reserve_Botanique (
+CREATE TABLE ReserveBotanique (
     PRIMARY KEY (IDreservbota),
-    IDreservbota NUMBER(),
+    IDreservbota NUMBER(10),
     Nomreserv    VARCHAR2(42),
     Ville        VARCHAR2(100),
     Pays         VARCHAR2(100),
@@ -93,12 +106,12 @@ CREATE TABLE Zone_Geo_Origine (
     IDEspeces VARCHAR2(42)
 );
 
-ALTER TABLE Emplacement ADD FOREIGN KEY (IDreservbota) REFERENCES Reserve_Botanique (IDreservbota);
+ALTER TABLE Emplacement ADD FOREIGN KEY (IDreservbota) REFERENCES  (IDreservbota);
 
-ALTER TABLE Especes ADD FOREIGN KEY (Nom_Famille) REFERENCES Famille_Especes (Nom_Famille);
+ALTER TABLE Especes ADD FOREIGN KEY (Nom_Famille) REFERENCES  (Nom_Famille);
 ALTER TABLE Especes ADD FOREIGN KEY (IDPlante) REFERENCES Plante (IDPlante);
 
-ALTER TABLE IS_A ADD FOREIGN KEY (IDreservbota) REFERENCES Reserve_Botanique (IDreservbota);
+ALTER TABLE IS_A ADD FOREIGN KEY (IDreservbota) REFERENCES  (IDreservbota);
 ALTER TABLE IS_A ADD FOREIGN KEY (Nom_Orga) REFERENCES Jardins (Nom_Orga);
 
 ALTER TABLE Nourri ADD FOREIGN KEY (IDNutriments) REFERENCES Nutriment (IDNutriments);
