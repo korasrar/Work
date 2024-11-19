561507d73ae495eeeb2487a01174cfc27e8c252a
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-11-19 09:59:47 +0100

    tp9 de con

--- R101/TP9/1_4_petites_betes/petites_betes.py
+++ R101/TP9/1_4_petites_betes/petites_betes.py
@@ -16,7 +16,10 @@ def toutes_les_familles(pokedex):
         set: l'ensemble des familles représentées dans le pokedex
     """
     famillepokedex = set()
-    
+    for pokemon in pokedex:
+        if pokemon[1] not in famillepokedex:
+            famillepokedex.add(pokemon[1])
+    return famillepokedex
 
 def nombre_pokemons(pokedex, famille):
     """calcule le nombre de pokemons d'une certaine famille dans un pokedex
@@ -29,7 +32,11 @@ def nombre_pokemons(pokedex, famille):
     Returns:
         int: le nombre de pokemons d'une certaine famille dans un pokedex
     """
-    ...
+    nbpokemon = 0
+    for pokemon in pokedex:
+        if pokemon[1] == famille:
+            nbpokemon += 1
+    return nbpokemon
 
 def frequences_famille(pokedex):
     """Construit le dictionnaire de fréqeunces des familles d'un pokedex
@@ -42,7 +49,13 @@ def frequences_famille(pokedex):
         dict: un dictionnaire dont les clés sont le nom de familles (str)
         et la valeur associée est le nombre de représentants de la famille (int)
     """
-    ...
+    frqfamille = {}
+    for pokemon in pokedex:
+        if pokemon[1] in frqfamille:
+            frqfamille[pokemon[1]] += 1
+        else:
+            frqfamille[pokemon[1]] = 1
+    return frqfamille
 
 def dico_par_famille(pokedex):
     """Construit un dictionnaire dont les les clés sont le nom de familles (str)
@@ -57,7 +70,9 @@ def dico_par_famille(pokedex):
         dict: un dictionnaire dont les clés sont le nom de familles (str) et la valeur associée est
         l'ensemble (set) des noms des pokemons de cette famille dans le pokedex
     """
-    ...
+    dicofrequence = frequences_famille(pokedex).keys()
+
+print(dico_par_famille([('Bulbizarre', 'Plante'), ('Aeromite', 'Poison'), ('Abo', 'Poison')]))
 
 def famille_la_plus_representee(pokedex):
     """détermine le nom de la famille la plus représentée dans le pokedex
@@ -69,7 +84,14 @@ def famille_la_plus_representee(pokedex):
     Returns:
         str: le nom de la famille la plus représentée dans le pokedex
     """
-    ...
+    frqfamille = frequences_famille(pokedex)
+    famillemax = ""
+    nbmax = 0
+    for famille,frq in frqfamille.items():
+        if frq > nbmax:
+            famillemax = famille
+            nbmax = frq
+    return famille
 
 
 # ==========================
