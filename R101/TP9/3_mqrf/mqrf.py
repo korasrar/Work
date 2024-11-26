c6be3268fd1a8883893ea272b5dde255c5a1bf58
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-11-26 10:47:35 +0100

    v1 et v2

--- R101/TP9/3_mqrf/mqrf.py
+++ R101/TP9/3_mqrf/mqrf.py
@@ -2,6 +2,8 @@
 # ==========================
 # La maison qui rend fou
 # ==========================
+mqrf1 = {"Abribus":"Astus", "Jeancloddus":"Abribus", "Plexus":"Gugus","Astus":None, "Gugus":"Plexus", "Saudepus":None}
+
 
 def quel_guichet(mqrf, guichet):
     """Détermine le nom du guichet qui délivre le formulaire A-38
@@ -13,7 +15,12 @@ def quel_guichet(mqrf, guichet):
     Returns:
         str: le nom du guichet qui finit par donner le formulaire A-38
     """
-    ...
+    guichet_actuel = guichet
+    while mqrf[guichet_actuel] is not None:
+        print("Je vais au guichet "+mqrf[guichet_actuel])
+        guichet_actuel = mqrf[guichet_actuel]
+    print("C'est bon mon goat, le formulaire A_38 est guichet :")
+    return guichet_actuel
 
 
 def quel_guichet_v2(mqrf, guichet):
@@ -28,8 +35,15 @@ def quel_guichet_v2(mqrf, guichet):
         tuple: le nom du guichet qui finit par donner le formulaire A-38 et le nombre de
         guichets visités pour y parvenir
     """
-    ...
-
+    nbguichet = 0
+    guichet_actuel = guichet
+    while mqrf[guichet_actuel] is not None:
+        print("Je vais au guichet "+mqrf[guichet_actuel])
+        guichet_actuel = mqrf[guichet_actuel]
+        nbguichet += 1
+    print("C'est bon mon goat, le formulaire A_38 est guichet :")
+    return (guichet_actuel,nbguichet)
+print(quel_guichet_v2(mqrf1,"Jeancloddus"))
 
 def quel_guichet_v3(mqrf, guichet):
     """Détermine le nom du guichet qui délivre le formulaire A-38
@@ -45,5 +59,5 @@ def quel_guichet_v3(mqrf, guichet):
         S'il n'est pas possible d'obtenir le formulaire en partant du guichet de depart,
         cette fonction renvoie None
     """
-    ...
+    
 
