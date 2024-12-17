a75ff103c42c69e0a7b060456d6bef365a11935e
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-12-17 10:26:10 +0100

    api de con du truc

--- R101/TP11/weekend.py
+++ R101/TP11/weekend.py
@@ -1,3 +1,5 @@
+import api_matrice as mat
+
 # EX1 ---------------------------------
 
 week_end_mai = {'Pierre':[12,70,10],'Paul':[100],'Marie':[15],'Anna':[]}
@@ -14,7 +16,7 @@ def affiche_bilan_financier(week_end):
             print(f"{personne} doit recevoir {abs(prixarendre)}")
         else:
             print(f"{personne} doit verser {prixarendre}")
-print(affiche_bilan_financier(week_end_mai))
+#print(affiche_bilan_financier(week_end_mai))
 
 # EX2 ---------------------------------
 
@@ -26,7 +28,7 @@ def amour_reciproque(dico_amoureux):
         if personne == dico_amoureux[dico_amoureux[personne]] and (dico_amoureux[personne],personne) not in res :
             res.append((personne,dico_amoureux[personne]))
     return res
-print(amour_reciproque(ATM))
+#print(amour_reciproque(ATM))
 
 def soupirants(dico_amoureux,nom):
     res = []
@@ -34,9 +36,32 @@ def soupirants(dico_amoureux,nom):
         if nom == dico_amoureux[personne]:
             res.append(personne)
     return res
-print(soupirants(ATM,'Cesar'))
+#print(soupirants(ATM,'Cesar'))
 
 # EX3 ---------------------------------
+def matrice1():
+    """ définition d'une matrice pour les tests """
+    mat1 = mat.creer_matrice(3, 3)
+    mat.set_valeur(mat1,0,0,1)
+    mat.set_valeur(mat1,0,1,2)
+    mat.set_valeur(mat1,0,2,3)
+    mat.set_valeur(mat1,1,0,4)
+    mat.set_valeur(mat1,1,1,5)
+    mat.set_valeur(mat1,1,2,6)
+    mat.set_valeur(mat1,2,0,7)
+    mat.set_valeur(mat1,2,1,8)
+    mat.set_valeur(mat1,2,2,9)
+    return mat1
+
+matrice_1 = matrice1()
+print(matrice_1)
 
 def sous_matrice(matrice,nb_lignes,nb_colonnes,position_haut,position_gauche):
-    
+    sousmatrice = mat.creer_matrice(nb_lignes,nb_colonnes)
+    for i in range(nb_colonnes):
+        for j in range(nb_lignes):
+            mat.set_valeur(sousmatrice,j,i,matrice[position_gauche+j][position_haut+i])
+    return sousmatrice
+print(sous_matrice(matrice_1,2,2,1,0))
+
+
