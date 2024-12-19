99965517b75ac3185b34ad4f65132d3d1f47ef67
korasrar <celestin.maubert@etu-univ-orleans.fr>
2024-12-19 09:38:41 +0100

    caca enlever

--- R101/exo_debug.py
+++
@@ -1,5 +0,0 @@
-def fonction1(x):
-    a = 12
-    a = 3+x
-    return a
-print(fonction1(23))
--- R101/exo_debug2.py
+++
@@ -1,9 +0,0 @@
-def fonction2(x):
-    a = 12
-    a = 3+x
-    A = a/2
-    b = a
-    a = 5
-    b = b+1
-    return a
-print(fonction2(5))
